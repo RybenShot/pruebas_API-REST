adf9aee1b0c31be4e54a69a23e1eb4672264f96c
sebastian <sebastianjimenezroman@hotmail.com>
2025-07-13 19:02:45 +0200

    se ha actualizados la base de datos de objetos y se han añadido endpoints sobre objetos, getrandomObject, getObjectbyID y getListaObjetosFiltrered

--- controllers/inv_controller.js
+++ controllers/inv_controller.js
@@ -30,10 +30,10 @@ export class InvController {
         // llamamos al modelo para que nos devuelva los objetos iniciales del investigador
         const invObjects = await InvModel.getInvObjects({ id })
 
-        // si se ha encontrado exitosamente el investigador, lo devolvemos
+        // si se ha encontrado exitosamente los objetos del investigador, lo devolvemos
         if (invObjects) return res.json(invObjects)
 
-        // aqui llegara si no ha encontrado el investigador
+        // aqui llegara si no ha encontrado los objetos del investigador
         res.status(404).json({ message: 'Objetos del Investigador no encontrado' })
     }
 
