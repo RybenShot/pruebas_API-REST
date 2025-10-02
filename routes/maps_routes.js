547d4f3f101925d9d0797348d60f59961338d0be
sebastian <sebastianjimenezroman@hotmail.com>
2025-10-02 12:34:16 +0200

    nuevo endpoint de CONTROL para recuperar votaciones de usuarios sobre mapas y ajuste de conexion de interacciones Usuario x usuario

--- routes/maps_routes.js
+++ routes/maps_routes.js
@@ -11,36 +11,34 @@ mapsRouter.get('/previewMap', MapsController.getPreviewMap)
 // BUSCAMOS los enemigos del mapa
 mapsRouter.get('/enemies/:id',  MapsController.getAllEnemies)
 
+// PEDIMOS todos las votaciones sobre los mapas
+mapsRouter.get('/getAllVotesMaps',  MapsController.getAllVotesMap)
+
 // BUSCAMOS un mapa por la ID
 mapsRouter.get('/:id',  MapsController.getById)
 
 // PEDIMOS votacion de mapa de like y dislike
 mapsRouter.get('/likeDislike/:id', MapsController.getLikeDislike)
-
 // Edicion de mapa para votacion de like y dislike
 mapsRouter.post('/likeDislike', MapsController.postLikeDislike)
 
 // PEDIMOS media de tiempo estimado de mapa
 mapsRouter.get('/timeEstimated/:id', MapsController.getTimeEstimated)
-
 // votacion de tiempo estimadod e mapa
 mapsRouter.post('/timeEstimated', MapsController.postTimeEstimated)
 
 // PEDIMOS media de dificultad de mapa
 mapsRouter.get('/difficultyMap/:id', MapsController.getDifficultyMap)
-
 // votacion de dificultad de mapa
 mapsRouter.post('/difficultyMap', MapsController.postDifficultyMap)
 
 // PEDIMOS investigadores recomendados de un mapa
 mapsRouter.get('/invRecommended/:id', MapsController.getRecInv)
-
 // votacion de investigadores recomendados de un mapa
 mapsRouter.post('/invRecommended', MapsController.postRecInv)
 
 // get de los comentarios de un mapa
 mapsRouter.get('/comments/:id', MapsController.getComments)
-
 // post para comentario sobre un mapa
 mapsRouter.post('/comments', MapsController.postComment)
 
