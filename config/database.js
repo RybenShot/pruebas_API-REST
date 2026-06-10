34ef3f8ebb1dc99f621745b0489bba91656bff13
sebastian <sebastianjimenezroman@hotmail.com>
2026-06-10 02:55:45 +0200

    añadiendo mongo DB

--- config/database.js
+++ config/database.js
@@ -1,17 +1,8 @@
 import { connect } from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    const conn = await connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    
-    console.log(`MongoDB conectado: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error de conexión: ${error.message}`);
-    process.exit(1);
-  }
-};
+  await connect('mongodb://mongo:AQCnazcPAVXgkUBRZvFvPVlJPJsKADfi@acela.proxy.rlwy.net:10965')
+  console.log('MongoDB conectado')
+}
 
-export default connectDB;
+export default connectDB
